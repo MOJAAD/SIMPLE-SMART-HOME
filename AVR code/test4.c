caadb387691392e05c3195f810add5f76ee3a65a
MOJAAD <mojaad@outlook.com>
2020-05-25 13:59:37 +0430

    ALL OF PROJECT ADDED(created before having git)

---
+++ AVR code/test4.c
@@ -0,0 +1,239 @@
+
+#include <mega16a.h>
+#include <stdio.h>
+#include <delay.h>
+#include <alcd.h>
+
+flash char shift[4]= { 0xFE , 0xFD , 0xFB , 0xF7} ;
+flash int position[16] = { 1 , 2 , 3 , 10 , 4 , 5 , 6 , 11 , 7 , 8 , 9 , 12 , 13 , 0 , 14 , 15 }; 
+
+int keypad(void); 
+
+interrupt [EXT_INT2] void ext_int2_isr(void)
+{
+if(PINA.4==1){lcd_puts("Automood Disable");delay_ms(500);lcd_clear();PORTA.4=0;}
+else
+        {
+        lcd_puts("Automood Enable!");
+        delay_ms(500);
+        lcd_clear();
+        PORTA.4=1;
+        }
+}
+
+// Voltage Reference: AREF pin
+#define ADC_VREF_TYPE ((0<<REFS1) | (0<<REFS0) | (0<<ADLAR))
+
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+ADMUX=adc_input | ADC_VREF_TYPE;
+// Delay needed for the stabilization of the ADC input voltage
+delay_us(10);
+// Start the AD conversion
+ADCSRA|=(1<<ADSC);
+// Wait for the AD conversion to complete
+while ((ADCSRA & (1<<ADIF))==0);
+ADCSRA|=(1<<ADIF);
+return ADCW;
+}
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=Out Bit6=Out Bit5=In Bit4=Out Bit3=Out Bit2=Out Bit1=In Bit0=In 
+DDRA=(1<<DDA7) | (1<<DDA6) | (0<<DDA5) | (1<<DDA4) | (1<<DDA3) | (1<<DDA2) | (0<<DDA1) | (0<<DDA0);
+// State: Bit7=0 Bit6=0 Bit5=T Bit4=1 Bit3=0 Bit2=1 Bit1=T Bit0=T 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (1<<PORTA4) | (0<<PORTA3) | (1<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (1<<DDD3) | (1<<DDD2) | (1<<DDD1) | (1<<DDD0);
+// State: Bit7=P Bit6=P Bit5=P Bit4=P Bit3=0 Bit2=0 Bit1=0 Bit0=0 
+PORTD=(1<<PORTD7) | (1<<PORTD6) | (1<<PORTD5) | (1<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (0<<CS01) | (0<<CS00);
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Disconnected
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0<<AS2;
+TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (0<<CS21) | (0<<CS20);
+TCNT2=0x00;
+OCR2=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: On
+// INT2 Mode: Rising Edge
+GICR|=(0<<INT1) | (0<<INT0) | (1<<INT2);
+MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+MCUCSR=(1<<ISC2);
+GIFR=(0<<INTF1) | (0<<INTF0) | (1<<INTF2);
+
+// USART initialization
+// USART disabled
+UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+
+// ADC initialization
+// ADC Clock frequency: 500.000 kHz
+// ADC Voltage Reference: AREF pin
+// ADC Auto Trigger Source: ADC Stopped
+ADMUX=ADC_VREF_TYPE;
+ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (0<<ADPS1) | (1<<ADPS0);
+SFIOR=(0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
+
+// SPI initialization
+// SPI disabled
+SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+
+// TWI initialization
+// TWI disabled
+TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTB Bit 0
+// RD - PORTB Bit 1
+// EN - PORTB Bit 3
+// D4 - PORTB Bit 4
+// D5 - PORTB Bit 5
+// D6 - PORTB Bit 6
+// D7 - PORTB Bit 7
+// Characters/line: 16
+lcd_init(16);
+
+// Global enable interrupts
+#asm("sei")
+
+while (1)
+      {
+      int j=0,h=0,sum=0;
+      float a=0,temp;
+      char buffer_lcd[16];  
+      
+        for(j=1000;j>0;j/=10)
+                {
+                 h=keypad();    
+                 if(h==12){PORTA.6=0;break;}
+                 if((h==3) || (h==7) || (h==11) || (h==14) || (h==15)) {break;}
+                 else {
+                        h=position[h]; 
+                        sum=h*j+sum;
+                        }
+                }
+               if(sum==1377){PORTA.6=1;lcd_puts("Accept..welcome!");delay_ms(1000);lcd_clear();}
+               else
+                        {    
+                        if(h==12){}
+                        else
+                                {
+                                PORTA.7=1;
+                                lcd_puts("     FALSE!");
+                                delay_ms(1000);
+                                PORTA.7=0;
+                                lcd_clear();
+                                } 
+                        }
+                  a=read_adc(0);   
+                  temp=a/2.054;
+                  sprintf(buffer_lcd,"T=%3.1f",temp);
+                  lcd_gotoxy(4,1);
+                  lcd_puts(buffer_lcd);
+                  lcd_gotoxy(10,1);
+                  lcd_puts("C"); 
+                  lcd_gotoxy(0,0);
+                 if(temp<=25)
+                            {
+                            PORTA.3=0;
+                            }
+                  else
+                            {
+                            PORTA.3=1;
+                            }   
+      }
+}
+
+int keypad(void){
+float a=0;
+int i=0,column=0;
+//char buffer_lcd[16];
+
+while(1){
+                for  (i=0;i<4;i++){
+                PORTD = shift[i] ; 
+                delay_ms(50);   
+                if(PIND.4==0){column=0;while(PIND.4==0){}return i*4 + column;}
+                if(PIND.5==0){column=1;while(PIND.5==0){}return i*4 + column;}
+                if(PIND.6==0){column=2;while(PIND.6==0){}return i*4 + column;}
+                if(PIND.7==0){column=3;while(PIND.7==0){}return i*4 + column;}
+                }   
+                a=read_adc(1); 
+              //  sprintf(buffer_lcd,"%3.1f",a);
+             //   lcd_gotoxy(0,0);
+             //   lcd_puts(buffer_lcd); 
+               if(a<300){PORTC.0=1;}
+               if(a>=300){PORTC.0=0;} 
+     
+        }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+#SIMPLE-SMART-HOME-PROJECT
+
+this project simulated by proteus software
+and i have used ATMEGA16A microcontroller
+also some senors were used in this project
